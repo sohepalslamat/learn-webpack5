d392bf8e4f9dda356632467d610221bd4f02515b
Sohep Alslamat <sohep.alslamat91@gmail.com>
2021-11-20 21:19:16 +0300

    lesson-1

---
+++ index.js
@@ -0,0 +1,4 @@
+const element = document.createElement('h2')
+element.innerHTML = "hello webpack"
+
+document.body.appendChild(element)
---
+++ main.js
@@ -0,0 +1,5 @@
+console.log(element)
+const element1 = document.createElement('div')
+element1.innerHTML = "hello div"
+
+document.body.appendChild(element1)
