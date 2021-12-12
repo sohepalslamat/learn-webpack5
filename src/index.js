1d10bd3682ca44ebac51fafc4f0f4b1f127b9a9a
Sohep Alslamat <sohep.alslamat91@gmail.com>
2021-12-12 21:15:07 +0300

    add images loared

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './main.css'
 const element = document.createElement('h2')
 element.innerHTML = "hello webpack"
 element.classList.add('red')
+const img = document.createElement('img')
+img.src = require('./webpack.png')
 
 document.body.appendChild(element)
-// document.body.appendChild(el)
+document.body.appendChild(img)
