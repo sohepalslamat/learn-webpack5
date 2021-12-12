b2f160a2c7dea60072e4c6da2860a9face360354
Sohep Alslamat <sohep.alslamat91@gmail.com>
2021-12-12 21:20:53 +0300

    use import instead of require

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // import el from './index1'
+import src from './webpack.png'
 import './main.css'
 
 const element = document.createElement('h2')
 element.innerHTML = "hello webpack"
 element.classList.add('red')
 const img = document.createElement('img')
-img.src = require('./webpack.png')
+img.src = src
 
 document.body.appendChild(element)
 document.body.appendChild(img)
